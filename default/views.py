ab77ca28259136bc9854306906e2dffc2bc0f07a
johnclamy <johnclamy@gmail.com>
2026-03-01 23:58:46 +0000

    define the url pattern for the about page

--- default/views.py
+++ default/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'default/index.html')
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, 'default/about.html')
