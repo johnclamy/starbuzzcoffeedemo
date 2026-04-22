0b610684abbf1703327039904edaffcedd99a8b2
johnclamy <johnclamy@gmail.com>
2026-04-22 21:23:27 +0100

    add an img to the products list

--- products/views.py
+++ products/views.py
@@ -12,7 +12,8 @@ products = [
         'size': 'Small',
         'is_vaccinated': True,
         'descr': 'Apricot male puppy, playful and socialized. Parents are health tested and have great temperaments. Puppies come with a health guarantee.',
-        'price': 1495.00
+        'price': 1495.00,
+        'image_url': 'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F2YXBvb3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'
     },
     {
         'id': 2,
@@ -23,7 +24,8 @@ products = [
         'size': 'Small',
         'is_vaccinated': True,
         'descr': 'KC registered fawn pug puppies born in February, ready for new homes. Parents are health tested and have great temperaments. Puppies are socialized and come with a health guarantee.',
-        'price': 1400
+        'price': 1400,
+        'image_url': 'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHVsZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'
     },
     {
         'id': 3,
@@ -34,7 +36,8 @@ products = [
         'size': 'Large',
         'is_vaccinated': True,
         'descr': 'Gentle and loving neutered male, shy and requires patience to build confidence. He is good with other dogs and children. Dobby is looking for a patient and understanding home to thrive in.',
-        'price': 299.99
+        'price': 299.99,
+        'image_url': 'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW5hdG9saWFuJTIwU2V0dGluZ2VyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
     }
 ]
 
