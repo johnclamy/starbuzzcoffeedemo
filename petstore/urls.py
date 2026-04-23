744c66185331fa51afea5888ae5bf42e6e1bf3f6
johnclamy <johnclamy@gmail.com>
2026-04-23 21:16:48 +0100

    enable the server to serve the stored images

--- petstore/urls.py
+++ petstore/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.conf import settings
 
 
@@ -14,3 +15,6 @@ if settings.DEBUG:
     urlpatterns += [
         path("__reload__/", include("django_browser_reload.urls")),
     ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
